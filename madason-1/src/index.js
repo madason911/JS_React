1b592930ac070a70a6f621dca4b7e2584f06cd04
madason911 <31772534+madason911@users.noreply.github.com>
2021-04-29 15:13:36 +0300

    37 - store,state

--- madason-1/src/index.js
+++ madason-1/src/index.js
@@ -1,22 +1,21 @@
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './redux/state.js'
+import store from './redux/state.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {addNewMessage, updateNewMessageText} from './redux/state.js'
 
 let reRender = (state) => {
   ReactDOM.render(
     <React.StrictMode>
-      <App state={state} addNewMessage={addNewMessage} updateNewMessageText={updateNewMessageText}/>
+      <App state={store.getState()} addNewMessage={store.addNewMessage.bind(store)} updateNewMessageText={store.updateNewMessageText.bind(store)}/>
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-reRender(state);
+reRender(store.getState());
 
-subscribe(reRender)
+store.subscribe(reRender)
 
 reportWebVitals();
--- madason-1/src/redux/state.js
+++ madason-1/src/redux/state.js
@@ -1,48 +1,51 @@
-let reRender = () => {
-	console.log("stateIsChanged");
-}
-let state = {
-	dialogsPage: {
-		dialogs: [
-			{id:1, name: "Ivan"},
-			{id:2, name: "Maga"},
-			{id:3, name: "Sasha"},
-			{id:4, name: "Masha"},
-			{id:5, name: "Dasha"}
-		],
-		messages: [
-			{id:1, message:"Hello!"},
-			{id:2, message:"How are you?"},
-			{id:3, message:"How old are you?"}
-		],
-		newMessageText: "madason"},
-	profilePage: {
-		posts: [
-			{id:1, post:"Hello my dear friend!"},
-			{id:2, post:"Hello my dear friends!"},
-			{id:3, post:"Hello my dear friend!"},
-			{id:4, post:"Hello my dear friends!"},
-			{id:5, post:"Hello my dear teacher!"}
-		]}
-}
-
-export let addNewMessage = (newMessage) => {
-	let messageObj = {
-		id: 4,
-		message: newMessage
+let store = {
+	_callSubscriber() {
+		console.log("stateIsChanged");
+	},
+	_state: {
+		dialogsPage: {
+			dialogs: [
+				{id:1, name: "Ivan"},
+				{id:2, name: "Maga"},
+				{id:3, name: "Sasha"},
+				{id:4, name: "Masha"},
+				{id:5, name: "Dasha"}
+			],
+			messages: [
+				{id:1, message:"Hello!"},
+				{id:2, message:"How are you?"},
+				{id:3, message:"How old are you?"}
+			],
+			newMessageText: "madason"},
+		profilePage: {
+			posts: [
+				{id:1, post:"Hello my dear friend!"},
+				{id:2, post:"Hello my dear friends!"},
+				{id:3, post:"Hello my dear friend!"},
+				{id:4, post:"Hello my dear friends!"},
+				{id:5, post:"Hello my dear teacher!"}
+			]}
+	},
+	getState() {
+		return this._state;
+	},
+	addNewMessage(newMessage) {
+		let messageObj = {
+			id: 4,
+			message: newMessage
+		}
+		this._state.dialogsPage.messages.push(messageObj)
+		this._callSubscriber(this._state);
+		this._state.dialogsPage.newMessageText = '';
+	},
+	updateNewMessageText(newMessage) {
+		this._state.dialogsPage.newMessageText = newMessage;
+		this._callSubscriber(this._state);
+	},
+	subscribe(observer) {
+		this._callSubscriber = observer;
 	}
-	state.dialogsPage.messages.push(messageObj)
-	reRender(state);
-	state.dialogsPage.newMessageText = '';
 }
 
-export let updateNewMessageText = (newMessage) => {
-	state.dialogsPage.newMessageText = newMessage;
-	reRender(state);
-}
-
-export const subscribe = (observer) => {
-	reRender = observer;
-}
-
-export default state;
+export default store;
+window.store = store;
